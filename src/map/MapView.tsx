6dbfe63db3f6aa32da86ab5fd4254a48ab169023
shobham kishan <shobhamkishan17@gmail.com>
2026-05-27 19:30:20 +0530

    Fix Leaflet marker icons

--- src/map/MapView.tsx
+++ src/map/MapView.tsx
@@ -1,5 +1,29 @@
 import React, { useState } from 'react'
 
+import L from 'leaflet'
+
+import markerIcon2x from
+'leaflet/dist/images/marker-icon-2x.png'
+
+import markerIcon from
+'leaflet/dist/images/marker-icon.png'
+
+import markerShadow from
+'leaflet/dist/images/marker-shadow.png'
+
+delete (
+  L.Icon.Default.prototype as any
+)._getIconUrl
+
+L.Icon.Default.mergeOptions({
+
+  iconRetinaUrl: markerIcon2x,
+
+  iconUrl: markerIcon,
+
+  shadowUrl: markerShadow,
+})
+
 import {
   MapContainer,
   TileLayer,
